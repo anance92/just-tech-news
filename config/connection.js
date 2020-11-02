1448f89bd5d156cea637d8d885666861e52b5d5f
Alexandra <anance5@murraystate.edu>
2020-11-02 17:34:56 -0600

    setup

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 // import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-require('dotenv').config();
+require('dotenv').config({ path: './config/vars.env'});
 
 // create connection to our database, pass in your MySQL information for username and password
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
