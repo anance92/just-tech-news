c3443befc3de4c3e1ad6eb39ab432619d90ac6e8
Alexandra <anance5@murraystate.edu>
2020-11-07 11:03:55 -0600

    added partials and testing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const session = require('express-session');
-
+const helpers = require('./utils/helpers');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -21,7 +21,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
